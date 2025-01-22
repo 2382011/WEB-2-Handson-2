358c4ea576d54e5b9f003462e1ef8ceaf69e6d1a
2382011 <2382011@unai.edu>
2025-01-22 13:08:27 +0700

    Update 01-built-in-types.js

--- handson-2/dist/01-built-in-types.js
+++ handson-2/dist/01-built-in-types.js
@@ -5,7 +5,7 @@ let name = "John Doe";
 let is_active = true;
 let not_found = null;
 let not_defined = undefined;
-let anything = "Grace Eunice";
+let anything = "Elsa Yunita";
 anything = 30;
 // Type inference (Typescript akan menebak tipe data secara otomatis)
 let salary = 5000; // Typescript akan menganggap variabili ini sebagai number
